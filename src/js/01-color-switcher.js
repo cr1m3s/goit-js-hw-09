27ecab674dc25309057982dd367ec9330efc52b5
cr1m3s <parioleks@gmail.com>
2021-12-01 14:47:40 +0100

    1st

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+button_start = document.querySelector("[data-start='']");
+button_start.addEventListener('click', body_color);
+let timerId = null;
+
+function body_color() {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  button_start.disabled = true;
+}
+
+button_stop = document.querySelector("[data-stop='']");
+button_stop.addEventListener('click', reset_button);
+
+function reset_button() {
+  clearInterval(timerId);
+  button_start.disabled = false;
+}
