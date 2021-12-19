93e9f4320759f3f6d17e257e15bc3a3ad8c0c592
cr1m3s <parioleks@gmail.com>
2021-12-19 21:42:45 +0100

    fixed timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -5,7 +5,13 @@ const start_button = document.querySelector("[data-start='']");
 start_button.disabled = true;
 
 let timerId = null;
-let countdown;
+let countdown = 0;
+
+const form_days = document.querySelector("[data-days='']");
+const form_hours = document.querySelector("[data-hours='']");
+const form_minutes = document.querySelector("[data-minutes='']");
+const form_seconds = document.querySelector("[data-seconds='']");
+
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -13,37 +19,32 @@ const options = {
   minuteIncrement: 1,
   FormDate: 'U',
   onClose: function (selectedDates) {
-    if (selectedDates[0] < options.defaultDate) window.alert('Please choose a date in the future');
-    else start_button.disabled = false;
+    if (selectedDates[0] <= options.defaultDate) window.alert('Please choose a date in the future');
+    else {
+      start_button.disabled = false;
+      countdown = selectedDates[0].getTime();
+      function countdown_func() {
+        timerId = setInterval(() => {
+          const { days, hours, minutes, seconds } = convertMs(countdown - Date.now());
+          form_days.textContent = days;
+          form_hours.textContent = hours;
+          form_minutes.textContent = minutes;
+          form_seconds.textContent = seconds;
+          if (
+            Number(form_days.textContent) === 0 &&
+            Number(form_hours.textContent) === 0 &&
+            Number(form_minutes.textContent) === 0 &&
+            Number(form_seconds.textContent) === 0
+          )
+            clearInterval(timerId);
+        }, 1000);
+      }
+      start_button.addEventListener('click', countdown_func);
+    }
   },
 };
 
 let fp = flatpickr('input#datetime-picker', options);
-
-start_button.addEventListener('click', countdown_func);
-
-function countdown_func() {
-  countdown = fp.selectedDates[0].getTime() - Date.now();
-  start_button.disabled == true;
-
-  setTimeout(() => {
-    if (countdown > 0) {
-      update_info();
-      countdown--;
-    } else {
-      start_button.disabled = false;
-    }
-  }, 1000);
-}
-
-function update_info() {
-  {
-    document.querySelector("[data-days='']").textContent,
-      document.querySelector("[data-hours='']").textContent,
-      document.querySelector("[data-minutes='']").textContent,
-      document.querySelector("[data-seconds='']").textContent,
-} = convertMs(countdown);
-}
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
